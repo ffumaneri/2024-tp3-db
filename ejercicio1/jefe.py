771e608dcc8c2458bb1f69b5538d9464fea9bd0a
lautarowaigand <113936471+lautarowaigand@users.noreply.github.com>
2024-11-11 20:19:59 -0300

    commiteado

--- ejercicio1/jefe.py
+++ ejercicio1/jefe.py
@@ -1,5 +1,5 @@
-from sqlalchemy import Column, String, Date, Integer, Numeric
-
+from sqlalchemy import Column, String, Date, Integer, Numeric,ForeignKey
+from sqlalchemy.orm import Mapped, mapped_column, relationship
 from common import Base
 
 
@@ -7,6 +7,9 @@ class Jefe(Base):
     __tablename__ = 'jefe'
 
     ##TODO: Insertar acá las columnas id, nombre y la relación con oficina
+    id: Mapped[int] = mapped_column(primary_key=True)
+    nombre: Mapped[str] = mapped_column(String)
+    oficina: Mapped["Oficina"] = relationship(back_populates="jefe")
 
     def __init__(self, nombre):
         self.nombre = nombre
--- ejercicio1/oficina.py
+++ ejercicio1/oficina.py
@@ -1,5 +1,5 @@
-from sqlalchemy import Column, String, Date, Integer, Numeric
-
+from sqlalchemy import Column, String, Date, Integer, Numeric,ForeignKey
+from sqlalchemy.orm import Mapped, mapped_column, relationship
 from common import Base
 
 
@@ -7,6 +7,10 @@ class Oficina(Base):
     __tablename__ = 'oficina'
 
     ##TODO: Insertar acá las columnas id, nombre y id del jefe.
+    id: Mapped[int] = mapped_column(primary_key=True)
+    nombre: Mapped[str] = mapped_column(String)
+    jefe_id: Mapped[int] = mapped_column(ForeignKey("jefe.id"))
+    jefe: Mapped["Jefe"] = relationship(back_populates="oficina")
 
     def __init__(self, nombre, jefe):
         self.nombre = nombre
