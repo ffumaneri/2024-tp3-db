d5e98a5c4b0416a328173abe9a5ba0074034c0a8
PapitaConPure <33335038+PapitaConPure@users.noreply.github.com>
2024-11-13 23:06:53 -0300

    ej2

--- ejercicio2/__main__.py
+++ ejercicio2/__main__.py
@@ -1,21 +1,43 @@
-from datetime import date
+#from datetime import date
+#from .jefe import Jefe
+#from .oficina import Oficina
 
-from .jefe import Jefe
-from .oficina import Oficina
+from .aula import Aula
+from .alumno import Alumno
 from common import session_factory
 
-
 def create_data():
-    ##TODO: crear 5 alumnos
-    ##TODO: crear 2 aulas
-    pass #borrar esto cuando agreguen codigo
+    session = session_factory()
 
+    aula1 = Aula('Aula 1')
+    aula2 = Aula('Aula 2')
+
+    session.add(aula1)
+    session.add(aula2)
+
+    mauricio = Alumno('Mauricio', aula1)
+    benjamin = Alumno('Benjamín', aula1)
+    roberto  = Alumno('Roberto',  aula2)
+    pedro    = Alumno('Pedro',    aula2)
+    jose     = Alumno('José',     aula2)
+
+    session.add(mauricio)
+    session.add(benjamin)
+    session.add(roberto)
+    session.add(pedro)
+    session.add(jose)
+
+    session.commit()
+    session.close()
 
 def get_alumnos():
-    ##TODO: Hacer query para obtener todas las aulas
-    pass #borrar esto cuando agreguen codigo
-
-
+    session = session_factory()
+    
+    query = session.query(Alumno)
+    listaAlumnos = query.all()
+    session.close()
+    
+    return listaAlumnos
 
 if __name__ == "__main__":
     alumnos = get_alumnos()
