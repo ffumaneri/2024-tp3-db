5d6b457d417c7eab06e8bd25d01a2fae98b973ed
Juan Cruz Rene <102224698+JuancRene@users.noreply.github.com>
2024-11-14 14:53:16 -0300

    tp2 arreglado

--- ejercicio1/oficina.py
+++ ejercicio1/oficina.py
@@ -1,12 +1,17 @@
-from sqlalchemy import Column, String, Date, Integer, Numeric
-
+from sqlalchemy import Column, String, Integer, ForeignKey
+from sqlalchemy.orm import relationship
 from common import Base
 
 
 class Oficina(Base):
-    __tablename__ = 'oficina'
+    __tablename__ = "oficina"
 
     ##TODO: Insertar acá las columnas id, nombre y id del jefe.
+    id = Column(Integer, primary_key=True)
+    nombre = Column(String(50))
+    id_jefe = Column(Integer, ForeignKey("jefe.id"))
+
+    jefe = relationship("Jefe", back_populates="oficina", uselist=False)
 
     def __init__(self, nombre, jefe):
         self.nombre = nombre
--- ejercicio2/__main__.py
+++ ejercicio2/__main__.py
@@ -1,19 +1,41 @@
 from datetime import date
 
-from .jefe import Jefe
-from .oficina import Oficina
+from .aula import Aula
+from .alumno import Alumno
 from common import session_factory
 
 
 def create_data():
+    session = session_factory()
     ##TODO: crear 5 alumnos
+    juan = Alumno("Juan Terzano", aula1)
+    rodrigo = Alumno("Rodrigo Prettis", aula2)
+    ivan = Alumno("Ivan Zualet", aula1)
+    manuel = Alumno("Manuel Hernandez", aula2)
+    enzo = Alumno("Enzo Fernandez", aula1)
+
+    session.add(aula1)
+    session.add(aula2)
+    session.add(juan)
+    session.add(rodrigo)
+    session.add(ivan)
+    session.add(manuel)
+    session.add(enzo)
+
     ##TODO: crear 2 aulas
-    pass #borrar esto cuando agreguen codigo
+    aula1 = Aula("Aula 1")
+    aula2 = Aula("Aula 2")
+    
+    session.commit()
+    session.close()
 
 
 def get_alumnos():
     ##TODO: Hacer query para obtener todas las aulas
-    pass #borrar esto cuando agreguen codigo
+    session = session_factory()
+    query = session.query(Alumno)
+    session.close()
+    return query.all()
 
 
 
