469ffd2041931208c4270e52dc23e3416f6b64ad
GrimaldiKevin <104236678+GrimaldiKevin@users.noreply.github.com>
2024-11-16 09:50:36 -0300

    TP3 db entrega

--- ejercicio2/aula.py
+++ ejercicio2/aula.py
@@ -1,13 +1,17 @@
-from sqlalchemy import Column, String, Date, Integer, Numeric
-
+from sqlalchemy import Column, String, Integer
+from sqlalchemy.orm import relationship
 from common import Base
-
 
 class Aula(Base):
     __tablename__ = 'aula'
 
-    ##TODO: Insertar acá las columnas id, nombre y relacion con alumno
+    id = Column(Integer, primary_key=True)
+    nombre = Column(String(50), nullable=False)
+
+    alumnos = relationship("Alumno", back_populates="aula")
 
     def __init__(self, nombre):
-        self.name = nombre
-        
+        self.nombre = nombre
+
+    def __repr__(self):
+        return f"<Aula(id={self.id}, nombre='{self.nombre}')>"
